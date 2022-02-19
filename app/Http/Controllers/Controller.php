b0019a8cf163991aace8439fcf99d98b8575d244
Ryan <lcrob55@gmail.com>
2022-02-19 23:03:39 +0800

    feat: swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="For Asia Yo",
+ *      description="匯率轉換器",
+ *      @OA\Contact(
+ *          email="lcrob55@gmail.com"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- app/Http/Controllers/ExchangeCurrencyController.php
+++ app/Http/Controllers/ExchangeCurrencyController.php
@@ -7,6 +7,71 @@ use App\Services\ExchangeService;
 
 class ExchangeCurrencyController extends Controller
 {
+    /**
+     * @OA\Post(
+     *      path="/api/exchange",
+     *      tags={"Projects"},
+     *      summary="轉換匯率",
+     *      description="轉換匯率至小數點後兩位",
+     *      @OA\Parameter(
+     *         name="source_currency",
+     *         in="query",
+     *         description="使用幣別",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *      @OA\Parameter(
+     *         name="traget_currency",
+     *         in="query",
+     *         description="兌換幣別",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *      @OA\Parameter(
+     *         name="amount",
+     *         in="query",
+     *         description="兌換金額",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                  type="object",
+     *                  required={""},
+     *                  example={
+     *                      "result": "1,000.00"
+     *                  },
+     *                  @OA\Property(property="result",type="string",example="1,000.00",description="兌換結果"),
+     *            )
+     *         )
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                  type="object",
+     *                  required={""},
+     *                  example={
+     *                       "message": "Target Currency Not Found."
+     *                  },
+     *                  @OA\Property(property="message",type="string",example="Target Currency Not Found.",description="找不到要兌換的幣別"),
+     *            )
+     *         )
+     *       ),
+     *     )
+     */
     public function exchange(ExchangeCurrencyRequest $request, ExchangeService $exchangeService)
     {
         try {
